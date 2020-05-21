dfdfd90650004c4e43a973f4cf88b7a6ead8d87f
Aro Andriamaro <ni.aro.aina@gmail.com>
2020-05-21 17:52:08 +0200

    Mise en place du Router et ajout de style global basique

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as MyRouter } from "react-router-dom";
+
+import App from "./App";
+import { GlobalStyles } from "./Global.styles";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles />
+    <MyRouter>
+      <App />
+    </MyRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
