2f3f2bcbc7f95f13aa1c2824ea526b49f1c44da5
Aro Andriamaro <ni.aro.aina@gmail.com>
2020-06-06 00:59:43 +0200

    Styling des repository individuel et refactoring de App.js et index.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as MyRouter, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { myStore } from "./redux/store";
 import { HomePage, RepositoriesPage } from "./pages";
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route path="/search/:keywords" component={RepositoriesPage} />
-        <Route>
-          <h3>Not found</h3>
-        </Route>
-      </Switch>
+      <Provider store={myStore}>
+        <MyRouter>
+          <CssBaseline />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/search/:keywords" component={RepositoriesPage} />
+            <Route>
+              <h3>Not found</h3>
+            </Route>
+          </Switch>
+        </MyRouter>
+      </Provider>
     </div>
   );
 }
--- src/components/Repositories/Repositories.styles.jsx
+++ src/components/Repositories/Repositories.styles.jsx
@@ -1,3 +1,14 @@
 import styled from "styled-components";
+import FadeIn from "react-fade-in";
+import Paper from "@material-ui/core/Paper";
 
-export const RepositoriesContainer = styled.div``;
+export const RepositoriesContainer = styled(FadeIn)`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+export const RepositoryContainer = styled(Paper)`
+  margin: 1rem 0;
+  padding: 1rem;
+`;
